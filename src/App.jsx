78e85f75c380747ea133d9c1de876f17f9cbbb6d
devDiogo <diogo.wallaceferreira@gmail.com>
2024-09-29 20:22:03 -0300

    app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import Veiculos from './components/Veiculos';
+
+const App = () => {
+  return (
+    <div>
+      <Veiculos />
+    </div>
+  );
+};
+
+export default App;
